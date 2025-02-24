85e1895e5aa7880bbd054036f0e74b6de6ed1185
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-02-24 09:49:57 +0200

    Prompt user to enter an option and validation for the user data that was entered

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -9,12 +9,26 @@
 using namespace std;
 
 int main() {
+    // Variable declarations
+    int choice;
+    
     // User Option Menu
     cout << "1) Draw a horizontal line" << endl;
     cout << "2) Draw a vertical line" << endl;
     cout << "3) Draw a square" << endl;
     cout << "4) Draw a rectangle" << endl;
     cout << "5) Quit" << endl;
+    
+    // Prompt user to enter a choice
+    cout << "Enter your option: ";
+    cin >> choice;
+    
+    // Data validation from user. If it isn't valid prompt them to enter a choice until it is valid
+    while(choice > 5 || choice < 1) {
+        cout << "I need a choice between 1 and 5. If you want to quit enter 5: ";
+        cin >> choice;
+    }
+    
     
     return 0;
 }
