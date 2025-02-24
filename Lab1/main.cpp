3464c42afb5046c8e63f4acc60a448e96802b5e5
Marios Kyriacou <kyriacou.m6@live.unic.ac.cy>
2025-02-24 09:47:43 +0200

    Created the user menu and added a general comment block at the top

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,16 +1,20 @@
 //
-//  main.cpp
-//  Lab1
+//  Lab 1 Exercise: The Shape Menu
+//  Create a menu where it gives the user options in order to draw certain shapes
 //
-//  Created by Marios Kyriacou on 24/02/2025.
-//
+//  Created by Marios Kyriacou (U244N0037) on 24/02/2025.
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!\n";
+    // User Option Menu
+    cout << "1) Draw a horizontal line" << endl;
+    cout << "2) Draw a vertical line" << endl;
+    cout << "3) Draw a square" << endl;
+    cout << "4) Draw a rectangle" << endl;
+    cout << "5) Quit" << endl;
     
     return 0;
 }
